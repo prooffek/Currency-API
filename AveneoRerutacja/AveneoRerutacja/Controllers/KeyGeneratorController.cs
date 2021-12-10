e7c04f47d9cfd2d3fa6fd5590c9952d794397b43
prooffek <prooffek@gmail.com>
2021-12-10 16:43:11 +0100

    Add key generator controller

---
+++ AveneoRerutacja/AveneoRerutacja/Controllers/KeyGeneratorController.cs
@@ -0,0 +1,31 @@
+﻿using AveneoRerutacja.KeyGenerator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AveneoRerutacja.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class KeyGeneratorController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> GenerateNewKey()
+        {
+            AuthenticationKey newKey = new();
+
+            if (newKey == null) throw new NullReferenceException();
+            
+            if (string.IsNullOrEmpty(newKey.KeyValue)) throw new NullReferenceException();
+
+            return Ok(new AuthenticationKey().KeyValue);
+        }
+    }
+}
