1f05b7deaaeaa0cdae47dcb6e82084532b15999a
prooffek <prooffek@gmail.com>
2021-12-10 14:03:10 +0100

    Add tests for generating unique key values

---
+++ AveneoRerutacja/AveneoRekrutacja.Tests/AuthorisationKeyTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AveneoRekrutacja.Tests
+{
+    [TestFixture]
+    class AuthorisationKeyTests
+    {
+
+        [Test]
+        public void AuthenticationKey_ShouldCreateAnAuthenticationKeyObjectWithNotNullKeyValueAtribute()
+        {
+            var key = new AuthenticationKey();
+
+            Assert.That(key_1.KeyValue, Is.Not.Null);
+        }
+
+        [Test]
+        public void AuthenticationKey_ShouldCreateAnAuthenticationKeyObjectWithNotEmptyKeyValueAttribute()
+        {
+            var key = new AuthenticationKey();
+
+            Assert.That(key_1.KeyValue, Is.Not.Empty);
+        }
+
+        [Test]
+        public void AuthenticationKey_ShouldCreateAuthenticationKeyObjectsWithDifferentKeyValueAttributeValues()
+        {
+            var key_1 = new AuthenticationKey();
+            var key_2 = new AuthenticationKey();
+
+            Assert.That(key_1.KeyValue, Is.Not.EqualTo(key_2.KeyValue));
+        }
+
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(1000000)]
+        public void AuthenticationKey_ShouldCreateAuthenticationKeyObjectsWithUniqueKeyValueAttributeValue(int counter)
+        {
+            List<string> keyValues = PopulateAuthenticationKeyValuesList(counter);
+
+            Assert.That(keyValues, Is.Unique);
+        }
+
+        private List<string> PopulateAuthenticationKeyValuesList(int counter)
+        {
+            List<string> keyValues = new();
+
+            for (int i = 0; i < counter; i++)
+            {
+                keyValues.Add(new AuthenticationKey().KeyValue);
+            }
+
+            return keyValues;
+        }
+
+        [Test]
+        public void AuthenticationKey_ShouldReturnTrueIfKeyValuesAreIdentical()
+        {
+            AuthenticationKey key = new();
+            string externalKey = key.KeyValue;
+
+            bool actual = key.KeysAreEqual(externalKey);
+
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void AuthenticationKey_ShouldReturnFalseIfKeyValuesAreNotIdentical()
+        {
+            AuthenticationKey key = new();
+            string externalKey = new AuthenticationKey().KeyValue;
+
+            bool actual = key.KeysAreEqual(externalKey);
+
+            Assert.That(actual, Is.False);
+        }
+    }
+}
