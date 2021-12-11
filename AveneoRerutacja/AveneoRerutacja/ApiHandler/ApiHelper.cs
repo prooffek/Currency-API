06c259e7dddfb633f6576d09f3c8d616218c1aad
DESKTOP-M7PO8IV\Kamil <prooffek@gmail.com>
2021-12-12 00:15:18 +0100

    Add ApiHelper that initialises and handles httpClient

---
+++ AveneoRerutacja/AveneoRerutacja/ApiHandler/ApiHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace AveneoRerutacja.ApiHandler
+{
+    public static class ApiHelper
+    {
+        private static HttpClient _httpClient;
+
+        public static HttpClient GetClient(string headerValue = "application/json")
+        {
+            if (_httpClient == null)
+            {
+                _httpClient = new HttpClient();
+                _httpClient.BaseAddress = new Uri("https://sdw-wsrest.ecb.europa.eu/");
+                _httpClient.DefaultRequestHeaders.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(headerValue));
+            }
+
+            return _httpClient;
+        }
+
+        public static string SetRequestUrl(string sourceCurrency, string targetCurrency, string startDate, string endDate, string apiKey, string format = "jsondata")
+        {
+            return $"service/data/EXR/D.{sourceCurrency}.{targetCurrency}.SP00.A?startPeriod={startDate}&endPeriod={endDate}&format={format}";
+        }
+    }
+}
