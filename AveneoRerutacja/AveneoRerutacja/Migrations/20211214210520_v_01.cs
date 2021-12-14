84f8bc708669b6f9b564dd4c03a02c22230fd607
prooffek <prooffek@gmail.com>
2021-12-14 22:07:34 +0100

    Add migrations to db

---
+++ AveneoRerutacja/AveneoRerutacja/Migrations/20211214210520_v_01.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace AveneoRerutacja.Migrations
+{
+    public partial class v_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Code = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DailyRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateClassId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DailyRates_Dates_DateClassId",
+                        column: x => x.DateClassId,
+                        principalTable: "Dates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurrencyDailyRate",
+                columns: table => new
+                {
+                    CurrenciesId = table.Column<int>(type: "integer", nullable: false),
+                    DailyRatesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyDailyRate", x => new { x.CurrenciesId, x.DailyRatesId });
+                    table.ForeignKey(
+                        name: "FK_CurrencyDailyRate_Currencies_CurrenciesId",
+                        column: x => x.CurrenciesId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CurrencyDailyRate_DailyRates_DailyRatesId",
+                        column: x => x.DailyRatesId,
+                        principalTable: "DailyRates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyDailyRate_DailyRatesId",
+                table: "CurrencyDailyRate",
+                column: "DailyRatesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DailyRates_DateClassId",
+                table: "DailyRates",
+                column: "DateClassId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyDailyRate");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropTable(
+                name: "DailyRates");
+
+            migrationBuilder.DropTable(
+                name: "Dates");
+        }
+    }
+}
