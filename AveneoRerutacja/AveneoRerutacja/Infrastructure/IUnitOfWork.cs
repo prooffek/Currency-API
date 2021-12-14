3719970217c08f257d43219e2cf41968dc1fc328
prooffek <prooffek@gmail.com>
2021-12-15 00:19:19 +0100

    Make unit of work interface and class generic to allow handling different databases and dbContexts properly

--- AveneoRerutacja/AveneoRerutacja/Infrastructure/IUnitOfWork.cs
+++ AveneoRerutacja/AveneoRerutacja/Infrastructure/IUnitOfWork.cs
@@ -1,14 +1,17 @@
 ﻿using System.Threading.Tasks;
 using AveneoRerutacja.Dimension;
 using AveneoRerutacja.Domain;
+using AveneoRerutacja.KeyGenerator;
+using Microsoft.EntityFrameworkCore;
 
 namespace AveneoRerutacja.Infrastructure
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork<T> where T : DbContext
     {
         IGenericRepository<Currency> Currencies { get; }
         IGenericRepository<DateClass> Dates { get; }
         IGenericRepository<DailyRate> DailyRates { get; }
+        IGenericRepository<AuthenticationKey> AuthenticationKeys { get; }
         Task Save();
     }
 }
--- AveneoRerutacja/AveneoRerutacja/Infrastructure/UnitOfWork.cs
+++ AveneoRerutacja/AveneoRerutacja/Infrastructure/UnitOfWork.cs
@@ -1,15 +1,16 @@
 ﻿using System.Threading.Tasks;
 using AveneoRerutacja.Dimension;
 using AveneoRerutacja.Domain;
+using AveneoRerutacja.KeyGenerator;
 using Microsoft.EntityFrameworkCore;
 
 namespace AveneoRerutacja.Infrastructure
 {
-    public class UnitOfWork : IUnitOfWork
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : DbContext
     {
-        private readonly DbContext _context;
+        private readonly T _context;
 
-        public UnitOfWork(DbContext context)
+        public UnitOfWork(T context)
         {
             _context = context;
         }
@@ -19,7 +20,10 @@ namespace AveneoRerutacja.Infrastructure
         public IGenericRepository<DateClass> Dates => new GenericRepository<DateClass>(_context);
         
         public IGenericRepository<DailyRate> DailyRates => new GenericRepository<DailyRate>(_context);
-        
+
+        public IGenericRepository<AuthenticationKey> AuthenticationKeys =>
+            new GenericRepository<AuthenticationKey>(_context);
+
         public async Task Save()
         {
             await _context.SaveChangesAsync();
