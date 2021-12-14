16a798f9ba59276114cc2680c641c642ca7a6eb1
prooffek <prooffek@gmail.com>
2021-12-14 22:38:15 +0100

    Add migrations for AuthenticationKeyDb

---
+++ AveneoRerutacja/AveneoRerutacja/Migrations/KeyGeneratorDb/20211214212727_v_02.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace AveneoRerutacja.Migrations.KeyGeneratorDb
+{
+    public partial class v_02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Keys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Keys", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Keys");
+        }
+    }
+}
