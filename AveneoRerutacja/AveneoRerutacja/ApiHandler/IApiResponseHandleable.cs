a3a72d0cc4b205e30c14d4d7aedbb2cdf4bdd889
prooffek <prooffek@gmail.com>
2021-12-14 11:54:08 +0100

    Add API response hanfler interface

---
+++ AveneoRerutacja/AveneoRerutacja/ApiHandler/IApiResponseHandleable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using AveneoRerutacja.Dimension;
+using AveneoRerutacja.Domain;
+using Newtonsoft.Json.Linq;
+
+namespace AveneoRerutacja.ApiHandler
+{
+    public interface IApiResponseHandleable
+    {
+        public IList<DailyRate> GetDailyRates();
+    }
+}
