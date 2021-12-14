c0d9b8229264f09edf2cd547ee3eb45833e510fd
prooffek <prooffek@gmail.com>
2021-12-14 22:37:14 +0100

    Add dbContext for authentication key

---
+++ AveneoRerutacja/AveneoRerutacja/Data/AuthenticationKeyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AveneoRerutacja.KeyGenerator;
+using Microsoft.EntityFrameworkCore;
+
+namespace AveneoRerutacja.Data
+{
+    public class AuthenticationKeyDbContext : DbContext
+    {
+        public DbSet<AuthenticationKey> Keys { get; set; }
+
+        public AuthenticationKeyDbContext(DbContextOptions<AuthenticationKeyDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql();
+    }
+}
