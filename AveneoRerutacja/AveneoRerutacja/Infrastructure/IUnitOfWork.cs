e926fc9bc03b8c61b65da12e56594f51b74d9854
prooffek <prooffek@gmail.com>
2021-12-14 23:14:13 +0100

    Add unit of worky interface and implementing it class

---
+++ AveneoRerutacja/AveneoRerutacja/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using AveneoRerutacja.Dimension;
+using AveneoRerutacja.Domain;
+
+namespace AveneoRerutacja.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Currency> Currencies { get; }
+        IGenericRepository<DateClass> Dates { get; }
+        IGenericRepository<DailyRate> DailyRates { get; }
+        Task Save();
+    }
+}
---
+++ AveneoRerutacja/AveneoRerutacja/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AveneoRerutacja.Dimension;
+using AveneoRerutacja.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AveneoRerutacja.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Currency> Currencies => new GenericRepository<Currency>(_context);
+        
+        public IGenericRepository<DateClass> Dates => new GenericRepository<DateClass>(_context);
+        
+        public IGenericRepository<DailyRate> DailyRates => new GenericRepository<DailyRate>(_context);
+        
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
