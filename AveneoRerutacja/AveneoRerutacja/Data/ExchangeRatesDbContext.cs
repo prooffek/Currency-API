d83faf7ff94309ead15e50a519ced908783796c0
prooffek <prooffek@gmail.com>
2021-12-14 22:00:25 +0100

    Add ExchangeRateDbContext

---
+++ AveneoRerutacja/AveneoRerutacja/Data/ExchangeRatesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AveneoRerutacja.Dimension;
+using AveneoRerutacja.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AveneoRerutacja.Data
+{
+    public class ExchangeRatesDbContext : DbContext
+    {
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<DateClass> Dates { get; set; }
+        public DbSet<DailyRate> DailyRates { get; set; }
+
+        public ExchangeRatesDbContext(DbContextOptions<ExchangeRatesDbContext> options) : base(options)
+        {
+        }
+
+        public ExchangeRatesDbContext(DbContextOptionsBuilder optionsBuilder) => 
+            optionsBuilder.UseNpgsql();
+    }
+}
