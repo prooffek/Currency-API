85e1e4faf63c6aacfff578aab22f11dcec1242f3
prooffek <prooffek@gmail.com>
2021-12-16 18:15:56 +0100

    Add SourceCurrencyDto model

---
+++ AveneoRerutacja/AveneoRerutacja/Models/SourceCurrencyDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AveneoRerutacja.Models
+{
+    public class CreateSourceCurrencyDto
+    {
+        public string Name { get; set; }
+        
+        [Required]
+        public string Code { get; set; }
+    }
+    
+    public class UpdateSourceCurrencyDto : CreateSourceCurrencyDto
+    { }
+    
+    public class SourceCurrencyDto : CreateSourceCurrencyDto
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
