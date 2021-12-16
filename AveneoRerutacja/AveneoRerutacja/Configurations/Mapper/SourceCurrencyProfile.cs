44b41072b589d0cb1d711eedc596bf8179334da4
prooffek <prooffek@gmail.com>
2021-12-16 19:01:28 +0100

    Add dailyRates, DateClass, SourceCurrency, TargetCurrency profiles for automapper

---
+++ AveneoRerutacja/AveneoRerutacja/Configurations/Mapper/SourceCurrencyProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using AveneoRerutacja.Domain;
+using AveneoRerutacja.Models;
+
+namespace AveneoRerutacja.Configurations.Mapper
+{
+    public class SourceCurrencyProfile : Profile
+    {
+        public SourceCurrencyProfile()
+        {
+            CreateMap<Currency, CreateSourceCurrencyDto>().ReverseMap();
+            CreateMap<Currency, UpdateSourceCurrencyDto>().ReverseMap();
+            CreateMap<Currency, SourceCurrencyDto>().ReverseMap();
+        }
+    }
+}
