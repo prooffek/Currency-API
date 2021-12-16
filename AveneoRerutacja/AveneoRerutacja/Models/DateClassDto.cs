d5b41a3f2dee6769244c95a7bfd42b3a6fb54b94
prooffek <prooffek@gmail.com>
2021-12-16 18:15:39 +0100

    Add DateClassDto model

---
+++ AveneoRerutacja/AveneoRerutacja/Models/DateClassDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using AveneoRerutacja.Domain;
+
+namespace AveneoRerutacja.Models
+{
+    public class CreateDateClassDto
+    {
+        [Required]
+        public DateTime Date { get; protected init; }
+    }
+    
+    public class UpdateDateClassDto : CreateDateClassDto
+    { }
+    
+    public class DateClassDto : CreateDateClassDto
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
