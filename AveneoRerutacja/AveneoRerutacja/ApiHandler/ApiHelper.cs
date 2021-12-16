52807d1400333dc922b754348c7d187a6216024c
prooffek <prooffek@gmail.com>
2021-12-17 00:52:43 +0100

    Add searching for nearest working day in the past, if the startDate was a holiday

--- AveneoRerutacja/AveneoRerutacja/ApiHandler/ApiHelper.cs
+++ AveneoRerutacja/AveneoRerutacja/ApiHandler/ApiHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using AveneoRerutacja.Dimension;
 
 namespace AveneoRerutacja.ApiHandler
 {
@@ -24,6 +26,35 @@ namespace AveneoRerutacja.ApiHandler
         public static string SetRequestUrl(string sourceCurrency, string targetCurrency, string startDate, string endDate, string apiKey, string format = "jsondata")
         {
             return $"service/data/EXR/D.{sourceCurrency}.{targetCurrency}.SP00.A?startPeriod={startDate}&endPeriod={endDate}&format={format}";
+        }
+
+        public static async Task<string> GetResponseString(string sourceCurrency, string targetCurrency, DateClass startDate, DateClass endDate, string apiKey, string format = "jsondata")
+        {
+            GetClient();
+            string responseString = null;
+
+            do
+            {
+                using(HttpResponseMessage response = await _httpClient.GetAsync(SetRequestUrl(
+                          sourceCurrency, targetCurrency, startDate.ToString(),
+                              endDate.ToString(), apiKey
+                          )))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        responseString = await response.Content.ReadAsStringAsync();
+                        startDate.SetToPreviousDay();
+                    }
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        throw new ArgumentException("Wrong argument provided");
+                    }
+
+                }
+            } while (string.IsNullOrEmpty(responseString));
+
+            return responseString;
         }
     }
 }
