b672cab9e4ad7bb6173adf84e6844b477da802cc
prooffek <prooffek@gmail.com>
2021-12-17 01:40:51 +0100

    Add logging with Serilog

--- AveneoRerutacja/AveneoRerutacja/Program.cs
+++ AveneoRerutacja/AveneoRerutacja/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace AveneoRerutacja
 {
@@ -13,11 +16,32 @@ namespace AveneoRerutacja
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().WriteTo.File(
+                path: @"logs\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-mm-dd HH:mm:ss.fff zzz} {Level:u3} {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information).CreateLogger();
+
+            try
+            {
+                Log.Information("Application is starting...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+                
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
