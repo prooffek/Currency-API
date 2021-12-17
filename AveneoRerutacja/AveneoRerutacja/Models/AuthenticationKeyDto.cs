d3bd9b9b1b9dd127c7a7617a1fd9173ad6ca2e0d
prooffek <prooffek@gmail.com>
2021-12-17 20:55:49 +0100

    Add AuthenticationDto model

---
+++ AveneoRerutacja/AveneoRerutacja/Models/AuthenticationKeyDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AveneoRerutacja.Models
+{
+    public class CreateAuthenticationKeyDto
+    {
+        [Required]
+        public string KeyValue { get; set; }
+    }
+    
+    public class UpdateAuthenticationKeyDto : CreateAuthenticationKeyDto
+    { }
+    
+    public class AuthenticationKeyDto : CreateAuthenticationKeyDto
+    {
+        public int Id { get; set; }
+    }
+}
