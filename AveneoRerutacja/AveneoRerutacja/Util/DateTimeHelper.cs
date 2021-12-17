72447a81b90db5c0984042432e8891d086f02930
prooffek <prooffek@gmail.com>
2021-12-17 20:24:48 +0100

    Delete unused files of weatherForcast and DataTimeHelper

--- AveneoRerutacja/AveneoRerutacja/Util/DateTimeHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AveneoRerutacja.Util
-{
-    public static class DateTimeHelper
-    {
-        public static bool IsDateTimeNull(DateTime date) => date.ToString("yyyy-MM-dd") == "0001-01-01";
-    }
-}
